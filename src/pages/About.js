d4cb17a9e28a7bd7deb77608773c84a6b73da9de
VICTOR M OTTATI <victor.ottati@gmail.com>
2023-05-17 15:53:36 +0100

    UPDATE: About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './About.css';
 
 export const About = () => {
 	return (
-		<div>
-			<h2>About</h2>
+		<div className="about-container">
+			<h2 className="about-title">About Me</h2>
+			<h2 className="about-name">Eng. Victor M. Ottati</h2>
+			<h3 className="about-description">
+				Engineer, Photographer and Fullstack website developer with React JS
+				Node Js and Express
+			</h3>
+			<a
+				href={'https://vmog.net/'}
+				target="_blank"
+				rel="noreferrer"
+				className="link"
+			>
+				Website Link
+			</a>
 		</div>
 	);
 };
